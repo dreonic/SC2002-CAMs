cb4beecee142603347b75a98256e3dd7ee7f5463
Gillbert Wong <66062290+gillwong@users.noreply.github.com>
2023-11-10 16:52:57 +0800

    feat: adds class skeleton for domain package

---
+++ src/domain/Staff.java
@@ -0,0 +1,22 @@
+package domain;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import user.User;
+
+public class Staff extends User {
+    private Set<Camp> campsCreated;
+
+    public Staff(String userID, String faculty) {
+        super(userID, faculty);
+    }
+
+    public Set<Camp> getCamps() {
+        return new HashSet<Camp>(campsCreated);
+    }
+
+    public void addCamp(Camp camp) {
+        // TODO
+    }
+}
---
+++ src/domain/StudentController.java
@@ -0,0 +1,17 @@
+package domain;
+
+public class StudentController {
+    private Student student;
+
+    public StudentController(Student student) {
+        this.student = student;
+    }
+
+    public void register(Camp camp) {
+        // TODO
+    }
+
+    public void withdraw(Camp camp) {
+        // TODO
+    }
+}
