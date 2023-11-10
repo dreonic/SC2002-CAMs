cd1aff7a84fad2d6f4c27f7f980778125811482f
Gillbert Wong <66062290+gillwong@users.noreply.github.com>
2023-11-10 13:35:11 +0800

    feat: adds user.User skeleton

--- src/user/User.java
+++ src/user/User.java
@@ -2,20 +2,41 @@ package user;
 
 public class User {
     private String userID;
-    private String hashedPassword;
+    private String passwordHash;
     private String faculty;
     private String name;
 
     public User(String userID, String faculty) {
         this.userID = userID;
         this.faculty = faculty;
+        // TODO: set default passwordHash to the hash value of "password"
+    }
+
+    public String getUserID() {
+        return userID;
     }
 
     public String getFaculty() {
         return this.faculty;
     }
 
+    public void setFaculty(String faculty) {
+        this.faculty = faculty;
+    }
+
     public String getName() {
         return this.name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPasswordHash() {
+        return passwordHash;
+    }
+
+    void setPasswordHash(String passwordHash) {
+        this.passwordHash = passwordHash;
     }
 }
