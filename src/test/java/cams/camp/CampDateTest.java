586647c8f56fe8c56c750e78f1a36e48d20d3a7f
Gillbert Wong <66062290+gillwong@users.noreply.github.com>
2023-11-17 22:14:10 +0800

    feat: adds unit test for CampDate

---
+++ src/test/java/cams/camp/CampDateTest.java
@@ -0,0 +1,40 @@
+package cams.camp;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+public class CampDateTest {
+    @Test
+    @DisplayName("Registration deadline cannot be after start date")
+    void registrationDeadlineAfterStartDateThrows() {
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MM-yyyy");
+        IllegalArgumentException e = assertThrows(IllegalArgumentException.class, () -> {
+            new CampDate(
+                    LocalDate.parse("17-11-2023", formatter),
+                    LocalDate.parse("20-11-2023", formatter),
+                    LocalDate.parse("18-11-2023", formatter));
+        });
+
+        assertEquals("registrationDeadline cannot be after startDate!", e.getMessage());
+    }
+
+    @Test
+    @DisplayName("Registration deadline cannot be after start date")
+    void startDateAfterEndDateThrows() {
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MM-yyyy");
+        IllegalArgumentException e = assertThrows(IllegalArgumentException.class, () -> {
+            new CampDate(
+                    LocalDate.parse("17-11-2023", formatter),
+                    LocalDate.parse("15-11-2023", formatter),
+                    LocalDate.parse("16-11-2023", formatter));
+        });
+
+        assertEquals("startDate cannot be after endDate!", e.getMessage());
+    }
+}
