793f04694e8db849a541d7d2f87250ce51373681
pascal pandey <pascalis.pandey@gmail.com>
2023-11-23 14:16:24 +0800

    javadoc for repliable

--- src/main/java/cams/repliable/Enquiry.java
+++ src/main/java/cams/repliable/Enquiry.java
@@ -2,28 +2,75 @@ package cams.repliable;
 
 import cams.domain.Student;
 
+/**
+ * The {@code Enquiry} class represents an inquiry made by a student.
+ * It extends the {@code Repliable} class, inheriting functionality related to replies.
+ * Each enquiry has a question and can have a corresponding reply.
+ * The student making the inquiry is associated with this enquiry.
+ *
+ * @author Gillbert Susilo Wong
+ * @author Juan Frederick
+ * @author Karl Devlin Chau
+ * @author Pascalis Pandey
+ * @author Trang Nguyen
+ * @version 1.0
+ * @since 2023-11-09
+ */
 public class Enquiry extends Repliable {
+    /**
+     * The question associated with this enquiry.
+     */
     private String question;
+
+    /**
+     * The reply to the enquiry.
+     */
     private String reply;
 
+    /**
+     * Constructs a new Enquiry object with the specified question and student.
+     *
+     * @param question The question associated with this enquiry.
+     * @param student  The student making the enquiry.
+     */
     public Enquiry(String question, Student student) {
         super(student);
         this.question = question;
-        reply = "";
+        this.reply = "";
     }
 
+    /**
+     * Gets the question associated with this enquiry.
+     *
+     * @return The question of this enquiry.
+     */
     public String getQuestion() {
         return question;
     }
 
+    /**
+     * Sets the question associated with this enquiry.
+     *
+     * @param question The new question for this enquiry.
+     */
     public void setQuestion(String question) {
         this.question = question;
     }
 
+    /**
+     * Gets the reply to this enquiry.
+     *
+     * @return The reply to this enquiry.
+     */
     public String getReply() {
         return reply;
     }
 
+    /**
+     * Sets the reply to this enquiry.
+     *
+     * @param reply The new reply for this enquiry.
+     */
     public void setReply(String reply) {
         this.reply = reply;
     }
--- src/main/java/cams/repliable/RepliableEditorInterface.java
+++ src/main/java/cams/repliable/RepliableEditorInterface.java
@@ -4,14 +4,54 @@ import cams.domain.Student;
 
 import java.util.List;
 
+/**
+ * The {@code RepliableEditorInterface} is the interface for {@code EnquiryEditor} and {@code SuggestionEditor}.
+ * It proides the basic functionality (methods and attributes) that a repliable editor should have.
+ *
+ * @author Gillbert Susilo Wong
+ * @author Juan Frederick
+ * @author Karl Devlin Chau
+ * @author Pascalis Pandey
+ * @author Trang Nguyen
+ * @version 1.0
+ * @since 2023-11-09
+ */
 public interface RepliableEditorInterface {
+    /**
+     * Creates a new repliable.
+     *
+     * @param content the content of the repliable being created.
+     * @param student the student that created the repliable.
+     */
     Repliable create(String content, Student student);
 
+    /**
+     * Edits a repliable.
+     *
+     * @param repliable the repliable that is being edited.
+     * @param newContent the new content that replaces the repliable's old content.
+     */
     void edit(Repliable repliable, String newContent);
 
+    /**
+     * Deletes a repliable.
+     *
+     * @param repliable the repliable that is being deleted.
+     */
     void delete(Repliable repliable);
 
+    /**
+     * Replies to a repliable with a reply message.
+     *
+     * @param repliable the repliable that is being replied to.
+     * @param replyMessage the reply message.
+     */
     void reply(Repliable repliable, Object replyMessage);
 
+    /**
+     * Views a list of repliable.
+     *
+     * @return list of enquiries.
+     */
     List<Repliable> view();
 }
